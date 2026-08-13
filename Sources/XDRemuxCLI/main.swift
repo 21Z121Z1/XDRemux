4ea9d984f695e3010def91ae0e7504fab0c0646b
21Z121Z1 <89170834+21Z121Z1@users.noreply.github.com>
2026-08-13 18:05:44 +0800

    cli: enable Motion Photo with Photographic Styles

--- Sources/XDRemuxCLI/main.swift
+++ Sources/XDRemuxCLI/main.swift
@@ -3,7 +3,9 @@ import XDRemuxAppleFeatures
 
 let arguments = Array(CommandLine.arguments.dropFirst())
 do {
-    if try MotionPhotoCLIIntegration.handleIfNeeded(arguments) {
+    if try MotionPhotoStylesCLIIntegration.handleIfNeeded(arguments) {
+        // The combined Motion Photo + Photographic Styles integration fully handled this command.
+    } else if try MotionPhotoCLIIntegration.handleIfNeeded(arguments) {
         // The Motion Photo integration fully handled this command.
     } else {
         XDRemuxCommand.main()
