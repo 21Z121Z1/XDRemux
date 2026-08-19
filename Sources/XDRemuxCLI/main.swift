0f5550c401200ce0008545e0cc23d29767720fd8
21Z121Z1 <89170834+21Z121Z1@users.noreply.github.com>
2026-08-19 10:07:39 +0800

    fix: keep parser control requests out of Motion Photo routing

--- Sources/XDRemuxCLI/main.swift
+++ Sources/XDRemuxCLI/main.swift
@@ -3,8 +3,23 @@ import Foundation
 import XDRemuxAppleFeatures
 
 let arguments = Array(CommandLine.arguments.dropFirst())
+let parserControlFlags: Set<String> = [
+    "--help",
+    "-h",
+    "--help-hidden",
+    "--generate-completion-script",
+    "--experimental-dump-help",
+]
+let isParserControlRequest = arguments.first == "help"
+    || arguments.contains(where: parserControlFlags.contains)
+
 do {
-    if try MotionPhotoCLIIntegration.handleIfNeeded(arguments) {
+    if isParserControlRequest {
+        // Help/completion requests belong entirely to ArgumentParser. In
+        // particular, do not let the Motion Photo batch pre-parser consume a
+        // command such as `batch --help` as if it were a conversion request.
+        XDRemuxRootCommand.main(arguments)
+    } else if try MotionPhotoCLIIntegration.handleIfNeeded(arguments) {
         // The Motion Photo integration fully handled this command.
     } else {
         XDRemuxRootCommand.main(arguments)
